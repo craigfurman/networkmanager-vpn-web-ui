8e38ab221c73e57576ee345a4a48cb7741546e7d
Craig Furman <craig.furman89@gmail.com>
2018-04-22 15:58:58 +0100

    Refresh connections every 5 seconds

--- public/main.js
+++ public/main.js
@@ -6,27 +6,6 @@ function onReady(fn) {
   }
 }
 
-onReady(function() {
-  var request = new XMLHttpRequest();
-  request.open('GET', '/api/connections', true);
-
-  request.onload = function() {
-    if (request.status == 200) {
-      var conns = JSON.parse(request.responseText);
-      var connList = document.getElementById('conn-list');
-      connList.innerHTML = connsHtml(conns);
-    } else {
-      throw new Error('error getting connections: ' + request.responseText);
-    }
-  };
-
-  request.onerror = function() {
-    // TODO
-  };
-
-  request.send();
-});
-
 function connHtml(conn) {
   var status = conn.active ? 'ON' : 'OFF';
   return '<div class="conn">' +
@@ -42,3 +21,43 @@ function connsHtml(conns) {
   }
   return s;
 }
+
+class Request {
+  constructor(method, url) {
+    this.method = method;
+    this.url = url;
+  }
+
+  make(onSuccess) {
+    var request = new XMLHttpRequest();
+    request.open(this.method, this.url, true);
+
+    request.onload = function() {
+      if (request.status == 200) {
+        onSuccess(request);
+      } else {
+        throw new Error('error making request: ' + request.responseText);
+      }
+    };
+
+    request.onerror = function() {
+      throw new Error('error attempting to make request');
+    };
+
+    request.send();
+  }
+}
+
+function refreshConns() {
+  var req = new Request('GET', '/api/connections');
+  req.make(function(xhr) {
+    var conns = JSON.parse(xhr.responseText);
+    var connList = document.getElementById('conn-list');
+    connList.innerHTML = connsHtml(conns);
+  });
+}
+
+onReady(function() {
+  refreshConns();
+  window.setInterval(refreshConns, 5000);
+});
