85613197f427cc65bd0a6f3182f4d4871266a16e
Craig Furman <craig.furman89@gmail.com>
2018-04-28 17:30:46 +0100

    Parameterise bind address for server

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/craigfurman/networkmanager-vpn-web-ui/vpnmanager"
 	"github.com/urfave/negroni"
 )
 
 func main() {
+	bindAddress := flag.String("bind-address", ":8080", "bind address")
+	flag.Parse()
+
 	api := vpnmanager.NewAPI(&vpnmanager.NmcliClient{})
 	server := negroni.Classic()
 	server.UseHandler(api)
-	server.Run(":8080") // TODO parameterise bind port/address
+	server.Run(*bindAddress)
 }
